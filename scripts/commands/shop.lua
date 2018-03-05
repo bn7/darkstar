e168bdef52f77513473eae1d7bf6c631ce960155
bn7 <email.steven.smith@gmail.com>
2018-03-05 11:15:18 -0700

    Update shop.lua

--- scripts/commands/shop.lua
+++ scripts/commands/shop.lua
@@ -38,25 +38,36 @@ function onTrigger(player,page)
     elseif (page == 2) then
         local stock_2 =
         {
-            -- Non Custom "for low lv noobs in exp party" food, same price as AH so non exploitable.
-            4376,     108,  -- Meat Jerky
-            -- Retail:  STR+3 INT-1 ATT23% (Cap:30)
-            5721,     500,  -- Crab Sushi
-            -- Retail:  VIT+1 Def+10 ACC+13% (Cap:64) "Resist Sleep"+1
-            4271,    2000,  -- Rice Dumpling
-            -- Retail:  HP+17 STR+3 VIT+2 AGI+1 ACC+5 ATT+20% (Cap:45) RATT+30% (Cap:45) HP/MP Healing +2 "Resist Paralyze"+4
-            4536,    3133,  -- Blackened Frog
-            -- Retail:  DEX+2 AGI+2 MND-2 ATT+14% (Cap:75) RATT+14% (Cap:75) EVA+5
-            --------------------------------------------------
-            -- Custom food items
-            4258,   32500,  -- Red Drop
-            4259,   32500,  -- Clear Drop
-            4260,   32500,  -- Green Drop
-            4261,   32500,  -- Yellow Drop
-            4262,   32500,  -- Purple Drop
-            4263,   32500,  -- Blue Drop
-            4264,   32500,  -- White Drop
-            4265,   4000    -- Black Drop
+        15762, 15000, -- Empress Band
+        15763, 35000, -- Emperor Band
+        4182, 15000, -- Instant Reraise
+        4165, 5000, -- Silent Oil
+        4164, 5000, -- Prism Powder
+        4151, 5000, -- Echo Drops
+		1127, 10000, -- Kindred's Seal
+        6307, 5000, -- Happo Shuriken +1 Pouch
+        5410, 5000, -- Virtue Stone pouch
+        4213, 5000, -- Icarus Wing
+        4175, 5000, -- Vile Elixir +1
+        5142, 5000, -- Bison Steak
+        4381, 5000, -- Meat Mithkabob
+        5146, 5000, -- Hedgehog Pie
+        5162, 5000, -- Squid Sushi +1
+        5163, 5000, -- Sole Sushi +1
+        5174, 5000, -- Tavnazian Taco
+        5759, 5000, -- Red Curry Bun
+        5781, 5000, -- Kitron Macaron
+        5718, 5000, -- Cream Puff
+        4558, 5000, -- Yagudo Drink
+        4511, 5000, -- Ambrosia
+        4284, 5000, -- Tender Navarin
+        4549, 5000, -- Dragon Soup
+        5603, 5000, -- Hydra Kofte +1
+        5925, 5000, -- Charred Salisbury Steak
+		1126, 5000, -- Beastmen's Seal
+		1550, 500000, -- Ark pentasphere
+		1875, 50000, -- Ancient beastcoin 
+        1298, 5000000, -- Hakutaku eye cluster
         };
         showShop(player, STATIC, stock_2);
         player:PrintToPlayer("Welcome to MogDonalds, Would you like fries with that?");
